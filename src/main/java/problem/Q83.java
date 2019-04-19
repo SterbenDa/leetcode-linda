9d3af897625b9fb7838d854b48f0632dc70b8433
SterbenDa <270801364@qq.com>
2019-04-19 18:04:01 +0800

    Q83 pref

--- src/main/java/problem/Q83.java
+++ src/main/java/problem/Q83.java
@@ -6,7 +6,21 @@ package problem;
  **/
 public class Q83 {
 
-    public ListNode deleteDuplicates(ListNode head) {
+    /**
+     * best
+     */
+    ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        head.next = deleteDuplicates(head.next);
+        return head.val == head.next.val ? head.next : head;
+    }
+
+    /**
+     * my
+     */
+    ListNode deleteDuplicates1(ListNode head) {
         if (head == null) {
             return null;
         }
